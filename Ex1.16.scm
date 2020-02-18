b18923108ee564137c4aa14e7d95c79667b96e41
Berken Tekin <berken.tekin@metu.edu.tr>
2020-02-18 17:34:16 +0000

    Add files via upload

---
+++ Ex1.16.scm
@@ -0,0 +1,6 @@
+(define (fast-expt b n)
+  (define (fast-expt-iter b counter product)
+    (cond ((= counter 0) product)
+          ((even? counter) (fast-expt-iter (* b b) (/ counter 2) product))
+          (else (fast-expt-iter b (- counter 1) (* product b)))))
+  (fast-expt-iter b n 1))
