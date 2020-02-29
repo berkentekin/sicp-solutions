a214d419c80bd92926636d7ca81a01f01cb6c1c9
Berken Tekin <berken.tekin@metu.edu.tr>
2020-02-29 18:01:20 +0000

    Create Ex2.01.scm

---
+++ Ex2.01.scm
@@ -0,0 +1,5 @@
+(define (make-rat n d)
+  (let ((g (gcd (abs n) (abs d))))
+  (cond ((and (< n 0) (< d 0)) (cons (/ (- n) g) (/ (- d) g)))
+        ((and (> n 0) (< d 0)) (cons (/ (- n) g) (/ (- d) g)))
+        (else (cons (/ n g) (/ d g))))))
