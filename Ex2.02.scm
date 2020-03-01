2a988cdafd3c623a654513725c0b08fdf038ed2d
Berken Tekin <berken.tekin@metu.edu.tr>
2020-03-01 02:47:47 +0000

    Add files via upload

---
+++ Ex2.02.scm
@@ -0,0 +1,23 @@
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+(define (make-segment pstart pend) (cons pstart pend))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (midpoint segment)
+  (define (average a b) (/ (+ a b) 2.0))
+  (let ((a (start-segment segment))
+        (b (end-segment segment)))
+    (make-point (average (x-point a) (x-point b))
+                (average (y-point a) (y-point b)))))
+
+(print-point (midpoint (make-segment (make-point 3 4) (make-point 5 7))))
