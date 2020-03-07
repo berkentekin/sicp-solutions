774449814db7378c194d72b18b84138559fbf8b2
Berken Tekin <berken.tekin@metu.edu.tr>
2020-03-07 05:16:42 +0000

    Add files via upload

---
+++ Ex2.05.scm
@@ -0,0 +1,18 @@
+(define (exp-iter n exp)
+  (define (exhelper n exp result)
+    (cond ((= exp 0) result)
+          ((even? exp) (exhelper (* n n) (/ exp 2) result))
+          (else (exhelper n (- exp 1) (* n result)))))
+  (exhelper n exp 1))
+
+(define (extractexp num baseofexp otherexp)
+  (define (expfinder x base result)
+    (if (= x 1) result (expfinder (/ x base) base (+ result 1))))
+  (if (= (remainder num otherexp) 0) (extractexp (/ num otherexp) baseofexp otherexp) (expfinder num baseofexp 0)))
+
+(define (my-cons a b) (* (exp-iter 2 a) (exp-iter 3 b)))
+(define (my-car z) (extractexp z 2 3))
+(define (my-cdr z) (extractexp z 3 2))
+
+(my-car (my-cons 4 2)) ; 4
+(my-cdr (my-cons 4 2)) ; 2
