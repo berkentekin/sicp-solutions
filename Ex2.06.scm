94cab15c0931e93e1b29c6d55271785e31ba389f
Berken Tekin <berken.tekin@metu.edu.tr>
2020-03-09 07:56:18 +0000

    Update Ex2.06.scm

--- Ex2.06.scm
+++ Ex2.06.scm
@@ -7,9 +7,9 @@
 
 ; Zero can be defined as (define (zero f) (lambda (x) x)) to make substitution easier.
 ; add-1 zero: (lambda (f) (lambda (x) (f ((zero f) x))))
-; (lambda (f) (lambda (x) (f (lambda (x) x) x)))
+; (lambda (f) (lambda (x) (f ((lambda (x) x) x))))
 (define one (lambda (f) (lambda (x) (f x)))) ;==> This is the function for one.
 
 ; add-1 one: (lambda (f) (lambda (x) (f ((one f) x))))
-; (lambda (f) (lambda (x) (f (lambda (x) (f x)) x)))
+; (lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
 (define two (lambda (f) (lambda (x) (f (f x))))) ;==> This is the function for two.
