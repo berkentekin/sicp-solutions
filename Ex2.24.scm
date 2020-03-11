1b2d4f932205c233f136ef07e076163885d69e74
Berken Tekin <berken.tekin@metu.edu.tr>
2020-03-11 18:16:31 +0000

    Update Ex2.24.scm

--- Ex2.24.scm
+++ Ex2.24.scm
@@ -4,3 +4,6 @@
         (else (+ (count-leaves (car ls)) (count-leaves (cdr ls))))))
 
 (count-leaves (list 1 (list 2 (list 3 4)))) ; 4
+
+; It would take too much time to convert the box and tree notation for the computer and I'm lazy.
+; However, in the future I may upload a picture of the solutions.
