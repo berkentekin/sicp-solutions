9e214f06938b84f5970480c51dcdb1525205f8b0
Berken Tekin <berken.tekin@metu.edu.tr>
2020-03-11 18:36:45 +0000

    Create Ex2.26.scm

---
+++ Ex2.26.scm
@@ -0,0 +1,8 @@
+; I solved these with pen & paper and so should you.
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y) ; (1 2 3 4 5 6)
+(cons x y) ; ((1 2 3) 4 5 6)
+(list x y) ; ((1 2 3) (4 5 6))
