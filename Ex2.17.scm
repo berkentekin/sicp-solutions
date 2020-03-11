076b978487d65d7f60b4a4dc5ea280175433ef84
Berken Tekin <berken.tekin@metu.edu.tr>
2020-03-11 07:40:34 +0000

    Add files via upload

---
+++ Ex2.17.scm
@@ -0,0 +1,8 @@
+(define nil '())
+
+(define (last-pair ls)
+  (cond ((null? (car ls)) (error "List is empty"))
+        ((null? (cdr ls)) (car ls))
+        (else (last-pair (cdr ls)))))
+
+(last-pair (list 1 3 4 5)) ;5
